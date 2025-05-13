9073c385bcf3687e5cc8d183fa6a652740d8407a
Kai Ruan <2313788275@qq.com>
2025-05-13 17:53:20 +0800

    Update example.py

--- example.py
+++ example.py
@@ -3,13 +3,13 @@ from swarmbench import SwarmFramework
 
 if __name__ == '__main__':
     name = 1
-    for level in ('Transport', 'Pursuit'): ########## Task Names, avaliable: {'Transport', 'Pursuit', 'Synchronization', 'Foraging', 'Flocking'}
+    for task in ('Transport', 'Pursuit'): ########## Task Names, avaliable: {'Transport', 'Pursuit', 'Synchronization', 'Foraging', 'Flocking'}
         for model in ('gpt-4o-mini', 'gpt-3.5-turbo'): ########### Models
             for seed in (27, 42):
                 SwarmFramework.submit(
                     f'exp_{name}',
                     SwarmFramework.model_config(model, 'YOUR_API_KEY', 'YOUR_API_BASE'), ########## API
-                    level,
+                    task,
                     log_dir='YOUR_LOG_DIR', ########## Logging
                     num_agents=10,
                     max_round=100,
