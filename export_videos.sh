456fbb72ef481f860151006d989ff563a84cbcac
Kai Ruan <2313788275@qq.com>
2025-05-14 20:16:56 +0800

    Update export_videos.sh

--- export_videos.sh
+++ export_videos.sh
@@ -1,4 +1,4 @@
 for i in {01..05}; do
     rm ./latex_output -rf
-    python ./analysis/generate_replay_videos.py --log-dir experiment_v$i -v --create-gif --gif-dpi 240 --gif-width 1280 --gif-dither none --force-recompile 
+    python ./analysis/generate_replay_videos.py --log-dir experiment_v$i -v --create-gif --gif-file v$i.gif --gif-dpi 240 --gif-width 1280 --gif-dither none --force-recompile
 done
